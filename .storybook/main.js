0864fe366534be6cf67b36d160f9e0b89a46f304
emmaroche <20088680@mail.wit.ie>
2024-03-07 14:57:22 +0000

    Home page components.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,16 +2,23 @@ module.exports = {
   "stories": [
     "../src/stories/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
   },
+
   "features": {
     "storyStoreV7": true
+  },
+
+  docs: {
+    autodocs: true
   }
 }
