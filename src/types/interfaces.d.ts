a0f06a1d9629cd920238710312f7a09910e58c39
emmaroche <20088680@mail.wit.ie>
2024-03-07 15:17:15 +0000

    Movie Details Page rendering.

--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -1,29 +1,39 @@
 export interface BaseMovie {
-    title: string;
-    budget: number;
-    homepage: string | undefined;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    overview: string;
-    release_date: string;
-    vote_average: number;
-    popularity: number;
-    poster_path?: string;
-    tagline: string;
-    runtime: number;
-    revenue: number;
-    vote_count: number;
-    favourite?: boolean;
+  title: string;
+  budget: number;
+  homepage: string | undefined;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  popularity: number;
+  poster_path?: string;
+  tagline: string;
+  runtime: number;
+  revenue: number;
+  vote_count: number;
+  favourite?: boolean;
 }
 
 export interface BaseMovieList {
-    movies: BaseMovie[];
-}   
+  movies: BaseMovie[];
+}
 
 export interface MovieT extends BaseMovie {
-    genres: {
-      id: number;
-      name: string;
-    }[];
-  }
+  genres: {
+    id: number;
+    name: string;
+  }[];
+}
+
+export interface MovieImage {
+  file_path: string;
+  aspect_ratio?: number; //some props are optional...
+  height?: number;
+  iso_639_1?: string;
+  vote_average?: number;
+  vote_count?: number;
+  width?: number;
+}
