eda7afef5f9cb5d5e31f4f7516c0261f31559ebe
emmaroche <20088680@mail.wit.ie>
2024-03-22 15:57:47 +0000

    Show movie review excerpts; Refactor Details page.

--- src/pages/movieDetailsPage.tsx
+++ src/pages/movieDetailsPage.tsx
@@ -1,83 +1,34 @@
-import React, {useState, useEffect}  from "react"; // replace existing react import
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails";
-import Grid from "@mui/material/Grid";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import { MovieT, MovieImage } from "../types/interfaces";
-import { getMovie, getMovieImages } from "../api/tmdb-api";
+import PageTemplate from "../components/templateMoviePage";
+import { getMovie } from "../api/tmdb-api";
+import { MovieT } from "../types/interfaces"; 
 
-const styles = {
-  imageListRoot: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  gridListTile: { 
-    width: "100%",
-    height: "auto",
-  },
 
-};
-
-interface MoviePageProps {
-  movie: MovieT;
-  images: MovieImage[];
-}
-
-const MoviePage: React.FC= () => {
-  const { id } = useParams();
-  const [movie, setMovie] = useState<MovieT>();
-  const [images, setImages] = useState<MovieImage[]>([]);
+const MovieDetailsPage: React.FC = (prop) => { 
+  const { id } = useParams<{ id: string }>(); 
+  const [movie, setMovie] = useState<MovieT | null>(null); 
 
   useEffect(() => {
-    getMovie(id ?? "").then((movie) => {
-      setMovie(movie);
-    });
+    if (id) { 
+      getMovie(id).then((movieData) => { 
+        setMovie(movieData);
+      });
+    }
   }, [id]);
-
-  useEffect(() => {
-    getMovieImages(id ?? "").then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   return (
     <>
       {movie ? (
-        <>
-          <MovieHeader {...movie} />
-          <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid item xs={3}>
-              <div >
-                <ImageList sx={styles.imageListRoot} cols={1}>
-                  {images.map((image) => (
-                    <ImageListItem
-                      key={image.file_path}
-                      sx={styles.gridListTile}
-                      cols={1}
-                    >
-                     <img
-                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                        alt={'Image alternative'}
-                      />                    
-                    </ImageListItem>
-                  ))}
-                </ImageList>
-              </div>
-            </Grid>
-            <Grid item xs={9}>
-              <MovieDetails {...movie} />
-            </Grid>
-          </Grid>
-        </>
+        <PageTemplate movie={movie}>
+          <MovieDetails movie={movie} />
+        </PageTemplate>
       ) : (
-        <h2>Waiting for API data</h2>
+        <p>Waiting for movie details</p>
       )}
     </>
   );
 };
 
-export default MoviePage;
+export default MovieDetailsPage;
