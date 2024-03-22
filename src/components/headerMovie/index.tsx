c4969b16da822c5acff4e538d363bc4171bc6b9c
emmaroche <20088680@mail.wit.ie>
2024-03-22 13:43:48 +0000

    Exercise - Show favourite icon on Movie Details page.

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import FavIcon from "@mui/icons-material/Favorite"; 
 import { MovieT } from "../../types/interfaces"; 
 
 const styles = {
@@ -18,12 +19,25 @@ const styles = {
 };
 
 const MovieHeader: React.FC<MovieT> = (props) => {
-  
+  const [isFavourite, setIsFavourite] = useState(false);
+
+  useEffect(() => {
+    // Checking if the movie is tagged as a favorite
+    const favourites = JSON.parse(localStorage.getItem("favourites") || "[]");
+    setIsFavourite(favourites.some((fav: MovieT) => fav.id === props.id));
+  }, [props.id]);
+
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
+
+      {isFavourite && (
+        <IconButton aria-label="favorite">
+          <FavIcon color="primary"  fontSize="large"/>
+        </IconButton>
+      )}
 
       <Typography variant="h4" component="h3">
         {props.title}{"   "}
@@ -33,6 +47,7 @@ const MovieHeader: React.FC<MovieT> = (props) => {
         <br />
         <span>{`${props.tagline}`} </span>
       </Typography>
+
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
